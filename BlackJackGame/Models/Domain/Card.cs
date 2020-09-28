9a00e1c89d5ecdc33eab25b5c82087818f3b7cca
Jonas De Corte <jonas.decorte@student.hogent.be>
2020-09-28 14:31:53 +0200

    Added DCD

---
+++ BlackJackGame/Models/Domain/Card.cs
@@ -0,0 +1,19 @@
+﻿namespace BlackJackGame.Models
+{
+    public class Card
+    {
+
+        #region Properties
+        public FaceValue FaceValue { get; }
+        public Suit Suit { get; }
+        #endregion
+
+        #region Constructors
+        public Card(Suit suit, FaceValue faceValue)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+    }
+}
---
+++ BlackJackGame/Models/Domain/Hand.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlackJackGame.Models
+{
+    public class Hand
+    {
+
+        #region Fields
+        private readonly IList<BlackJackCard> _cards;
+        #endregion
+
+        #region Properties
+        public IEnumerable<BlackJackCard> Cards { get; }
+
+        public int NrOfCards { get; }
+
+        public int Value { get; }
+        #endregion
+
+        #region Constructors
+        public Hand()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+        #region Methods
+        public void AddCard(BlackJackCard blackJackCard)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TurnAllCardsFaceUp()
+        {
+            throw new System.NotImplementedException();
+        }
+        private int CalculateValue()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
